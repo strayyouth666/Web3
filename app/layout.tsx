3f71b692bae49b58634876b7ab01d93b8c13cdb7
76687383+strayyouth666@users.noreply.github.com <76687383+strayyouth666@users.noreply.github.com>
2024-10-02 20:48:38 +0700

    Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,23 +5,24 @@ import Head from 'next/head'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Solana Template App',
-  description:
-    'Start your Solana journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.',
+  title: 'FLXTHESIXTH',
+  icons: '',
+  // description:
+  //   'Start your Solana journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.',
   twitter: {
-    title: 'Solana Template App',
-    description:
-      'Start your Solana journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.',
-    images: 'https://solana-web3-template.nightly.app/preview.png',
+    title: 'FLXTHESIXTH',
+    // description:
+    //   'Start your Solana journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.',
+    // images: 'https://solana-web3-template.nightly.app/preview.png',
     card: 'summary_large_image',
-    site: '@nightly_app',
+    // site: '@nightly_app',
   },
   openGraph: {
-    title: 'Solana Template App',
-    description:
-      'Start your Solana journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.',
-    images: 'https://solana-web3-template.nightly.app/preview.png',
-    url: 'https://solana-web3-template.nightly.app',
+    title: 'FLXTHESIXTH',
+    // description:
+    //   'Start your Solana journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.',
+    // images: 'https://solana-web3-template.nightly.app/preview.png',
+    // url: 'https://solana-web3-template.nightly.app',
     type: 'website',
   },
 }
